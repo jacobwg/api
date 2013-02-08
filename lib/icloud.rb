4e5dcfcfb8e343baad7cb9139a3438e70a235fe7
Jacob Gillespie <mail@jacobwg.com>
2013-02-08 16:07:34 -0600

    Returning location in a new format

--- lib/icloud.rb
+++ lib/icloud.rb
@@ -2,7 +2,13 @@ class Icloud
 
   def self.current_location
     @@r ||= Rosumi.new(ENV['ICLOUD_EMAIL'], ENV['ICLOUD_PASSWORD'])
-    @@r.updateDevices.last['location']
+    location = @@r.updateDevices.last['location']
+    location = {
+      latitude: location['latitude'],
+      longitude: location['longitude'],
+      accuracy: location['horizontalAccuracy'],
+      timestamp: location['timeStamp']
+    }
   end
 
 end
