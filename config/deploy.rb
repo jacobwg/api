4fab723dbf30c7c8d622d0f920fa6001be509744
Jacob Gillespie <mail@jacobwg.com>
2013-02-09 13:38:27 -0600

    Now updating just the right part of the crontab

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,6 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-    run "cd #{current_path} && whenever -w api"
+    run "cd #{current_path} && whenever -i api"
   end
 end
