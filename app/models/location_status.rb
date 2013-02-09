ce2264d8167df4720ebd6e131079e93d4ae1114a
Jacob Gillespie <mail@jacobwg.com>
2013-02-09 14:06:17 -0600

    Now using symbols

--- app/models/location_status.rb
+++ app/models/location_status.rb
@@ -4,10 +4,10 @@ class LocationStatus < ActiveRecord::Base
   def self.store_current_location
     location = Icloud.current_location
 
-    status = self.where(time: DateTime.strptime("#{location['timestamp']}", '%Q'),).first_or_create(
-      latitude: location['latitude'],
-      longitude: location['longitude'],
-      accuracy: location['horizontalAccuracy'])
+    status = self.where(time: DateTime.strptime("#{location[:timestamp]}", '%Q'),).first_or_create(
+      latitude: location[:latitude],
+      longitude: location[:longitude],
+      accuracy: location[:horizontalAccuracy])
     status.save
   end
 end
