d4fe2973f1bd99c0cfbc145bf4b0293297684d50
Jacob Gillespie <mail@jacobwg.com>
2013-05-25 15:50:30 -0500

    Removed unnecessary stuff - no more location history

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var iCloud = require('./lib/icloud'),
-    Firebase = require('firebase');
     _ = require('underscore'),
     moment = require('moment'),
     nconf = require('nconf');
@@ -8,9 +7,7 @@ nconf.argv()
      .env()
      .file({ file: 'config.json' });
 
-var tracker = new iCloud(nconf.get('ICLOUD_USERNAME'), nconf.get('ICLOUD_PASSWORD')),
-    db = new Firebase('https://jacob-gillespie.firebaseio.com');
-
+var tracker = new iCloud(nconf.get('ICLOUD_USERNAME'), nconf.get('ICLOUD_PASSWORD'));
 
 var express = require('express');
 var app = express();
@@ -31,39 +28,6 @@ app.get('/v1/location', function(req, res) {
   });
 });
 
-app.get('/v1/location/history', function(req, res) {
-  db.child('location/history').on('value', function(snap) {
-    res.jsonp(snap.val());
-  });
-});
-
 var port = process.env.PORT || 3000;
 app.listen(port);
-
-locations = require('./locations');
-
-_.each(locations, function(location, idx) {
-  return;
-  var latitude = location.latitude;
-  var longitude = location.longitude;
-  var time = moment(location.time);
-
-  latitude = Math.ceil(latitude * 1000) / 1000;
-  longitude = Math.ceil(longitude * 1000) / 1000;
-  id = 'location/history/y' + time.year() + '/w' + time.isoWeek() + '/l' + (latitude + ':' + longitude).replace(/[.]/g, '_');
-
-  db.child(id).transaction(function(count) {
-    if (count)
-      return count + 1;
-    else
-      return 1;
-  });
-
-  console.log(idx / 18837);
-
-  //db.child('locations').push(location);
-});
-
-console.log('Server started on port 3000');
-
-
+console.log('Server started on port ' + port);
